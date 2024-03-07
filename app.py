b31fea3406c5722ab7b39c823c527f1974365285
Asep Hendar <4sep.hendar@gmail.com>
2024-03-07 19:51:12 +0700

    Membuat method POST & operator tambahan

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/tambahan', methods=['POST'])
+def Addition():
+    a = request.json['a']
+    b = request.json['b']
+    
+    hasil = a + b
+    return jsonify({"results" : hasil})
+
+if __name__ == '__main__':
+    app.run(debug=True)
